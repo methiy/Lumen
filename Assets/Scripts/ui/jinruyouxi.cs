cd2b19b5867b6f2e7d8e48e1565b5490828e452b
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-04-05 21:28:04 +0800

    ui

--- Assets/Scripts/ui/jinruyouxi.cs
+++ Assets/Scripts/ui/jinruyouxi.cs
@@ -9,11 +9,25 @@ public class Choose : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         this.GetComponent<Button>().onClick.AddListener(OnClick);
+       
+    }
+
+        // 调用延迟加载方法
+
+    
+
+    // 延迟加载场景的方法
+    IEnumerator LoadSceneAfterDelay()
+    {
+        // 等待两秒
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene("11");
     }
 
     void OnClick()
     {
-        SceneManager.LoadScene("11");
+        StartCoroutine(LoadSceneAfterDelay());
+        
     }
 
     // Update is called once per frame
