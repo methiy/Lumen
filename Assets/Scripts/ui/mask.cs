e1f1800b7cf4b5ffcb3d517a83a3d0acfb7f39f9
xiaotu1234 <jp2021212994@qmul.ac.uk>
2024-04-10 18:15:54 +0800

    ui

---
+++ Assets/Scripts/ui/mask.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mask : MonoBehaviour
+{
+    public GameObject maska;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 pos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       pos.z=0;
+        if(Input.GetMouseButton(0))
+        {
+            Instantiate(maska,pos,Quaternion.identity,transform);
+        }
+    }
+}
