2b28127c9437021e995be88fe6c65d288a035a2b
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-05-03 15:07:34 +0800

    ui，传了个字体于是多了好多不知道啥的文件

---
+++ Assets/Scripts/ui/tujian.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIMovement : MonoBehaviour
+{
+    public float moveDistance = 153f; // 移动距离
+    public float moveSpeed = 5f; // 移动速度
+    public float buttonCooldown = 1f; // 按钮冷却时间
+
+    private RectTransform rectTransform;
+    private bool canInteract = true;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        // 获取左右按钮并绑定点击事件
+        Button leftButton = GameObject.Find("LeftButton").GetComponent<Button>();
+        leftButton.onClick.AddListener(MoveLeft);
+
+        Button rightButton = GameObject.Find("RightButton").GetComponent<Button>();
+        rightButton.onClick.AddListener(MoveRight);
+    }
+
+    // 向左移动游戏对象
+    void MoveLeft()
+    {
+        if (canInteract)
+        {
+            MoveObject(-moveDistance);
+            StartCoroutine(ButtonCooldown());
+        }
+    }
+
+    // 向右移动游戏对象
+    void MoveRight()
+    {
+        if (canInteract)
+        {
+            MoveObject(moveDistance);
+            StartCoroutine(ButtonCooldown());
+        }
+    }
+
+    // 移动游戏对象
+    void MoveObject(float distance)
+    {
+        Vector3 targetPosition = rectTransform.position + new Vector3(distance, 0f, 0f);
+        StartCoroutine(MoveCoroutine(targetPosition));
+    }
+
+    // 协程实现平滑移动
+    IEnumerator MoveCoroutine(Vector3 targetPosition)
+    {
+        while (Vector3.Distance(rectTransform.position, targetPosition) > 0.01f)
+        {
+            rectTransform.position = Vector3.MoveTowards(rectTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // 按钮冷却协程
+    IEnumerator ButtonCooldown()
+    {
+        canInteract = false;
+        yield return new WaitForSeconds(buttonCooldown);
+        canInteract = true;
+    }
+}
