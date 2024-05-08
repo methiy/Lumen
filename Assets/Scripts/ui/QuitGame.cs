2b4d7590df825170a10632d8aa7c01b646be980d
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-05-08 22:37:08 +0800

    ui

--- Assets/Scripts/ui/QuitGame.cs
+++ Assets/Scripts/ui/QuitGame.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI; 
 public class QuitGame : MonoBehaviour
 {
-     void OnClick()
+    void Start()
     {
-        Debug.Log("exit");
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+
+    }
+    void OnClick()
+    {
         Application.Quit(); 
     }
 }
